9258c83d4944829e70bbdb00d75b7dbe2edf5d56
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-15 17:41:29 +0200

    Update NotifySupervisor.php

--- Listeners/NotifySupervisor.php
+++ Listeners/NotifySupervisor.php
@@ -4,7 +4,7 @@ namespace Lumis\LeaveApplication\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Notification;
-use Lumis\LeaveApplication\Events\LeaveCreated;
+use Lumis\LeaveApplication\Events\LeaveReturn;
 use Lumis\LeaveApplication\Notifications\LeaveApprovalNotification;
 
 class NotifySupervisor implements ShouldQueue
@@ -14,10 +14,10 @@ class NotifySupervisor implements ShouldQueue
     /**
      * Handle the event.
      *
-     * @param LeaveCreated $event
+     * @param LeaveReturn $event
      * @return void
      */
-    public function handle(LeaveCreated $event): void
+    public function handle(LeaveReturn $event): void
     {
         $leave = $event->leave;
         $supervisor = $leave->staff->supervisor;
