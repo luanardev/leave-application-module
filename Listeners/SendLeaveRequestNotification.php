cfd461e8b68d9c59424ce284ed7e77a277799954
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-15 17:41:34 +0200

    Update SendLeaveRequestNotification.php

--- Listeners/SendLeaveRequestNotification.php
+++ Listeners/SendLeaveRequestNotification.php
@@ -5,7 +5,8 @@ namespace Lumis\LeaveApplication\Listeners;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Notification;
 use Lumis\LeaveApplication\Events\LeaveCreated;
-use Lumis\LeaveApplication\Notifications\LeaveRequestNotification;
+use Lumis\LeaveApplication\Events\LeaveReturn;
+use Lumis\LeaveApplication\Notifications\LeaveReturnNotification;
 
 class SendLeaveRequestNotification implements ShouldQueue
 {
@@ -21,7 +22,7 @@ class SendLeaveRequestNotification implements ShouldQueue
         $leave = $event->leave;
         $staff = $leave->staff;
 
-        $notification = new LeaveRequestNotification($leave, $staff);
+        $notification = new LeaveReturnNotification($leave, $staff);
         Notification::send($staff, $notification);
 
     }
